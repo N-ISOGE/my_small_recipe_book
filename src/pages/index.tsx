dd6c0b6fbb49a79ccb4dc73b0b5a108e45132ca9
sang hyeon, son <daseeni@gmail.com>
2023-07-14 14:57:22 +0900

    link 컴포넌트 사용해보기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { Link } from "gatsby";
 
 const Greeting = (props: { name: string }) => {
   return <p> Hi {props.name}!</p>;
@@ -8,11 +9,15 @@ const Greeting = (props: { name: string }) => {
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
+      <Link to="/map">지도</Link>
+
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <Greeting name="5" />
       <Greeting name="파폭" />
       <Greeting name="엣지" />
+
+      <Link to="/map">지도</Link>
     </main>
   );
 };
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { Link } from "gatsby";
 
 const MapPage: React.FC<PageProps> = () => {
   return (
     <main>
+      <Link to="/">대문</Link>
+
       <h1>개요</h1>
       <p>뭐 올릴지 정하는 중</p>
+
+      <Link to="/">대문</Link>
     </main>
   );
 };
