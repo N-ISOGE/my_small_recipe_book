997e19cdab9cd2d60f643ac979f6ad83cca7a8c3
sang hyeon, son <daseeni@gmail.com>
2023-07-14 14:45:16 +0900

    컴포넌트 함수 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 
+const Greeting = (props: { name: string }) => {
+  return <p> Hi {props.name}!</p>;
+};
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Greeting name="5" />
+      <Greeting name="파폭" />
+      <Greeting name="엣지" />
     </main>
   );
 };
