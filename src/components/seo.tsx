a308022ad638841c770458977884fa54e902c690
daseeni <daseeni@gmail.com>
2024-01-07 13:22:25 +0000

    seo.tsx 문제되는 함수 제거

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,27 +1,25 @@
 import React from "react";
-// import { useStaticQuery, graphql } from "gatsby";
+import { graphql } from "gatsby";
 
-
-
-// const data = useStaticQuery(graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `);
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
 
 type SeoProps = {
   title: string;
 };
 
-const Seo = ({ title }: SeoProps) => {
+const Seo = ({ title}: SeoProps) => {
+
   return (
     <title>
-      {title} 
-       | {/*data.site.siteMetadata?.title /*|| "empty title"*/}
+      {title} | {/*data.site.siteMetadata?.title /*|| "empty title"*/}
     </title>
   );
 };
