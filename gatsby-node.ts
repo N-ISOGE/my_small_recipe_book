c479b54061f8b3ab81ca4edfb32858d0dd238d5d
daseeni <daseeni@gmail.com>
2024-01-07 13:22:25 +0000

    seo.tsx 문제되는 함수 제거
    modified:   gatsby-node.ts
    modified:   src/components/seo.tsx

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -16,9 +16,9 @@ exports.onCreateWebpackConfig = ({ actions }: any) => {
     resolve: {
       plugins: [new TsconfigPathPlugin()],
       // alias: {
-      //     // "@components": path.resolve(__dirname, "./src/components"),
-      //     // "@images": path.resolve(__dirname, "./src/images"),
-      //     // "@pages": path.resolve(__dirname, "./src/pages"),
+      //     "@components": path.resolve(__dirname, "./src/components"),
+      //     "@images": path.resolve(__dirname, "./src/images"),
+      //     "@pages": path.resolve(__dirname, "./src/pages"),
       // }
     },
   });
