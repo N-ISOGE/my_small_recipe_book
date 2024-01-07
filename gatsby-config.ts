fdb4b14e59721a4755b996850253ea5f234ac564
daseeni <daseeni@gmail.com>
2024-01-07 13:21:55 +0000

    gatsby config 플러그인 정리

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,7 @@ module.exports = {
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: "My small recipe book"
+    title: "My small recipe book",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -19,14 +19,38 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-styled-components",
+    // "gatsby-plugin-typescript",
     "gatsby-plugin-tsconfig-paths",
+    //
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-dts-css-modules",
+    "gatsby-plugin-sass",
+    //
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-styled-components",
+    //
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        "@components": path.join(__dirname, "src/components"),
+        "@pages": path.join(__dirname, "src/pages"),
+        "@images": path.join(__dirname, "src/images"),
+      },
+    },
+    //
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -57,25 +81,6 @@ const config: GatsbyConfig = {
       },
       __key: "blog",
     },
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        extensions: [".mdx", ".md"],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-root-import",
-      options: {
-        "@components": path.join(__dirname, "src/components"),
-        "@pages": path.join(__dirname, "src/pages"),
-        "@images": path.join(__dirname, "src/images"),
-      },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-dts-css-modules",
-    "gatsby-plugin-sass",
   ],
 };
 
