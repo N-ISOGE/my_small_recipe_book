64f4eddc828c3950142ba6b844d7424baadc9887
daseeni <daseeni@gmail.com>
2024-01-17 06:07:47 +0000

    폴더 구조 변경

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -52,21 +52,6 @@ const config: GatsbyConfig = {
       },
     },
     //
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     name: "main",
-    //     path: "./",
-    //   },
-    //   __key: "main",
-    // },
-    // {
-    //   resolve: "gatsby-plugin-page-creator",
-    //   options: {
-    //     path: "./",
-    //   },
-    //   __key: "main",
-    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -86,14 +71,14 @@ const config: GatsbyConfig = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "blog",
-        path: "blog/",
+        path: "./src/blog/",
       },
       __key: "blog",
     },
     {
       resolve: "gatsby-plugin-page-creator",
       options: {
-        path: "blog/",
+        path: "./src/blog/",
       },
       __key: "blog",
     },
