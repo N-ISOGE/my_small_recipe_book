24288a7f1b9dd4a1b4bfa028b4d1a651c8570250
daseeni <daseeni@gmail.com>
2024-01-21 03:51:30 +0000

    문서 서식 지정

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -3,12 +3,12 @@ import { toString } from "mdast-util-to-string";
 import type { RemarkPlugin } from "@astrojs/markdown-remark";
 
 export const remarkReadingTime: RemarkPlugin = () => {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return function (tree: unknown, { data }) {
-		const textOnPage = toString(tree);
-		const readingTime = getReadingTime(textOnPage);
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-expect-error
-		data.astro.frontmatter.minutesRead = readingTime.text;
-	};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return function (tree: unknown, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
 };
