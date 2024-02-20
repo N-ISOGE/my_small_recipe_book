f5a9aba0709e42391488e8db85beced97ca864e1
sang hyeon, son <daseeni@gmail.com>
2024-02-21 00:58:28 +0900

    Fix menulink (#56)
    * 메뉴 링크 수정
    
    * 0.9.6
    
    * 메뉴링크에서 홈 경로 수정
    
    * 0.9.7

--- src/site.config.ts
+++ src/site.config.ts
@@ -31,7 +31,7 @@ export const siteConfig: SiteConfig = {
 export const menuLinks: Array<{ title: string; path: string }> = [
     {
         title: "Home",
-        path: `${baseUrl}/`,
+        path: "_",
     },
     {
         title: "About",
