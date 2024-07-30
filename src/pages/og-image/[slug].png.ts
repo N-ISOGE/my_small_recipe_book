1a0c776b9a3e065cd6e02aaf0dc4e616cb4975cb
daseeni <daseeni@gmail.com>
2024-07-30 08:44:52 +0000

    cactus-theme v4.6.0 병합 및 패키지 설정 수정

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -1,12 +1,14 @@
 import type { APIContext, InferGetStaticPropsType } from "astro";
-import satori, { type SatoriOptions } from "satori";
-import { html } from "satori-html";
-import { Resvg } from "@resvg/resvg-js";
-import { siteConfig } from "@/site-config";
-import { getAllPosts, getFormattedDate } from "@/utils";
 
 import RobotoMono from "@/assets/roboto-mono-regular.ttf";
 import RobotoMonoBold from "@/assets/roboto-mono-700.ttf";
+
+import { getAllPosts } from "@/data/post";
+import { siteConfig } from "@/site-config";
+import { getFormattedDate } from "@/utils";
+import { Resvg } from "@resvg/resvg-js";
+import satori, { type SatoriOptions } from "satori";
+import { html } from "satori-html";
 //
 export const prerender = true;
 
